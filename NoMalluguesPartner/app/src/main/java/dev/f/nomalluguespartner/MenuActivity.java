31fd062e2261f8154ee74f8a7071008b30d7b6bf
joseluisrosasbaza <Bazno379!>
2015-03-08 00:37:21 -0600

    Fragments attempt: FAILED

--- NoMalluguesPartner/app/src/main/java/dev/f/nomalluguespartner/MenuActivity.java
+++ NoMalluguesPartner/app/src/main/java/dev/f/nomalluguespartner/MenuActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentActivity;
 import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
@@ -18,6 +19,9 @@ import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
 
 
 public class MenuActivity extends ActionBarActivity
@@ -37,6 +41,7 @@ public class MenuActivity extends ActionBarActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        setContentView(R.layout.fragment_encargos);
 
         mNavigationDrawerFragment = (NavigationDrawerFragment)
                 getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
@@ -46,23 +51,40 @@ public class MenuActivity extends ActionBarActivity
         mNavigationDrawerFragment.setUp(
                 R.id.navigation_drawer,
                 (DrawerLayout) findViewById(R.id.drawer_layout));
+
+        // Check that the activity is using the layout version with
+        // the fragment_container FrameLayout
+        if (findViewById(R.id.container) != null) {
+
+            // However, if we're being restored from a previous state,
+            // then we don't need to do anything and should return or else
+            // we could end up with overlapping fragments.
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            // Create a new Fragment to be placed in the activity layout
+            PlaceholderFragment firstFragment = new PlaceholderFragment();
+
+            // In case this activity was started with special instructions from an
+            // Intent, pass the Intent's extras to the fragment as arguments
+            firstFragment.setArguments(getIntent().getExtras());
+
+            // Add the fragment to the 'fragment_container' FrameLayout
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.encargos_fragment, firstFragment).commit();
+        }
     }
 
     @Override
     public void onNavigationDrawerItemSelected(int position) {
         // update the main content by replacing fragments
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction()
-                .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
-                .commit();
-    }
 
-    public void onSectionAttached(int number) {
-        switch (number) {
+        switch (position) {
             case 1:
                 Intent a = new Intent(MenuActivity.this,MapsActivity.class);
                 startActivity(a);
-              //  mTitle = getString(R.string.title_section1);
+                //  mTitle = getString(R.string.title_section1);
                 break;
             case 2:
                 mTitle = getString(R.string.title_section2);
@@ -70,7 +92,16 @@ public class MenuActivity extends ActionBarActivity
             case 3:
                 mTitle = getString(R.string.title_section3);
                 break;
+            case 4:
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
+                        .commit();
         }
+    }
+
+    public void onSectionAttached(int number) {
+
     }
 
     public void restoreActionBar() {
